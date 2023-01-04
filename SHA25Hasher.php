19d7a87089aaf8c84ba32fc0e3769712c6412873
Franco Nicolás Placidi <79880040+francoplacidi@users.noreply.github.com>
2023-01-04 15:57:56 -0300

    Add files via upload
    Change the encryption type to sha256 in Laravel.

---
+++ SHA25Hasher.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Libraries;
+
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+use Illuminate\Hashing\AbstractHasher;
+
+class SHA25Hasher extends AbstractHasher implements HasherContract{
+
+    /**
+     * Hash the given value.
+     *
+     * @param  string  $value
+     * @return array   $options
+     * @return string
+     */
+    public function make($value, array $options = []) {    
+        return hash('sha256', $value);
+    }
+
+    /**
+     * Check the given plain value against a hash.
+     *
+     * @param  string  $value
+     * @param  string  $hashedValue
+     * @param  array   $options
+     * @return bool
+     */
+    public function check($value, $hashedValue, array $options = []) {
+        return $this->make($value) === $hashedValue;
+    }
+
+    /**
+     * Check if the given hash has been hashed using the given options.
+     *
+     * @param  string  $hashedValue
+     * @param  array   $options
+     * @return bool
+     */
+    public function needsRehash($hashedValue, array $options = []) {
+        return false;
+    }
+
+}
